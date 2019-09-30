3d20b2c6ffca28db1e1183fa9fd60ff55f30e735
tzuchienkao <oyan114@gmail.com>
2019-09-30 17:56:26 +0800

    build public

--- docs/practice/url_params.js
+++
@@ -1,8 +0,0 @@
-var url_params = window.location.search;
-var params = url_params.replace('?', '')
-var params = params.split('&')
-var ary = []
-for(i in params){
-    var split = params[i].split('=')
-    ary[split[0]] = decodeURIComponent(split[1])
-}
