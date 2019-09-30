f2947a480e3531c44c022a8d77c033240bfc0ce1
tzuchienkao <oyan114@gmail.com>
2019-09-30 17:49:03 +0800

    add vue todolist

---
+++ docs/practice/vue_to_do_list/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="page">
+    <orderTop @getItem="getItem" />
+    <orderList :newItem="newItem" />
+  </div>
+</template>
+
+<script>
+import orderTop from './components/orderTop.vue'
+import orderList from './components/orderList.vue'
+
+export default {
+  name: 'page',
+  components: {
+    orderTop,
+    orderList
+  },
+  data () {
+    return {
+      newItem: ''
+    }
+  },
+  methods: {
+    getItem(val) {
+      this.newItem = val;
+    }
+  }
+}
+</script>
